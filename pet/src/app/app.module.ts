cd185aa892bfbea9f0521367a5f37d461fdd0de5
keanh <keanh275@gmail.com>
2020-06-03 15:02:39 +0700

    add change color

--- pet/src/app/app.module.ts
+++ pet/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { ShowPetComponent } from './show-pet/show-pet.component';
 import { AwesomeCalculatorComponent } from './awesome-calculator/awesome-calculator.component';
 import {FormsModule} from '@angular/forms';
+import { ColorComponent } from './color/color.component';
 
 // @ts-ignore
 @NgModule({
   declarations: [
     AppComponent,
     ShowPetComponent,
-    AwesomeCalculatorComponent
+    AwesomeCalculatorComponent,
+    ColorComponent
   ],
     imports: [
         BrowserModule,
